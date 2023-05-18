e90b6aaba8d3950258e07edfd5eea3cb63b5257a
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-05-18 14:41:29 -0700

    defined half of mybind

--- practice-for-ch-js-argument-exercises-long-practice-main/classwork.js
+++ practice-for-ch-js-argument-exercises-long-practice-main/classwork.js
@@ -1,10 +1,33 @@
-function sum() {
+// function sum() {
+//     let total = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         total += arguments[i];
+//     }
+//     console.log(total);
+// }
+
+// sum(1, 2, 3, 4) === 10;
+// sum(1, 2, 3, 4, 5) === 15;
+
+function sum(...args) {
     let total = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        total += arguments[i];
+    for (let i = 0; i < args.length; i++) {
+        total += args[i];
     }
     console.log(total);
 }
 
+// function sum(...args) {
+//     let total = 0;
+//     for (let arg of args) total += arg;
+//         return total;
+// }
+
 sum(1, 2, 3, 4) === 10;
 sum(1, 2, 3, 4, 5) === 15;
+
+Function.prototype.myBind = function (context, arguments) {
+    (){
+
+    }
+}
